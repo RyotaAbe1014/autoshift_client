516a31c51aff85bad2d38e3543b11644bfc432e5
RyotaAbe1014 <bm1000bmmm@gmail.com>
2023-02-17 23:19:03 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="app" id="app">
+    <router-view />
+    <router-link to="/about">About</router-link>
+    <br>
+    <router-link to="/">Home</router-link>
+  </div>
+</template>
