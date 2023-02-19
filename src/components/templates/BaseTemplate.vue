9182b9e03b20111f4b9cb24fa14337e1d9b72bd8
RyotaAbe1014 <bm1000bmmm@gmail.com>
2023-02-19 21:42:19 +0900

    add: サイドバー実装

--- src/components/templates/BaseTemplate.vue
+++ src/components/templates/BaseTemplate.vue
@@ -8,9 +8,21 @@ const drawer = ref(true)
   <v-app id="inspire">
     <v-navigation-drawer v-model="drawer">
       <v-list dense>
-        <v-list-item prepend-icon="mdi-account" title="admin" value="admin"></v-list-item>
-        <v-list-item prepend-icon="" title="メンバー管理" value=""></v-list-item>
-        <v-list-item prepend-icon="" title="シフト管理" value=""></v-list-item>
+        <v-list-item prepend-icon="mdi-account" title="株式会社00" value="株式会社00"></v-list-item>
+        <v-list-group>
+          <template v-slot:activator="{ props }">
+            <v-list-item v-bind="props" prepend-icon="mdi-account-circle" title="メンバー管理"></v-list-item>
+          </template>
+          <v-list-item prepend-icon="" title="メンバー一覧" value="メンバー一覧"></v-list-item>
+          <v-list-item prepend-icon="" title="メンバー追加" value="メンバー追加"></v-list-item>
+        </v-list-group>
+        <v-list-group>
+          <template v-slot:activator="{ props }">
+            <v-list-item v-bind="props" prepend-icon="mdi-calendar-month" title="シフト管理"></v-list-item>
+          </template>
+          <!-- <v-list-item prepend-icon="" title="メンバー一覧" value="メンバー一覧"></v-list-item>
+          <v-list-item prepend-icon="" title="メンバー追加" value="メンバー追加"></v-list-item> -->
+        </v-list-group>
         <v-list-item prepend-icon="mdi-cog" title="設定" value="設定"></v-list-item>
         <v-list-item prepend-icon="mdi-logout" title="ログアウト" value="ログアウト"></v-list-item>
       </v-list>
