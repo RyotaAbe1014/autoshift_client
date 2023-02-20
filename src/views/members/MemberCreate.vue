7fad38072dd4e5f7e6faf367191d62e0c4c40e2d
RyotaAbe1014 <bm1000bmmm@gmail.com>
2023-02-21 01:06:00 +0900

    add: ユーザー作成機能

--- src/views/members/MemberCreate.vue
+++ src/views/members/MemberCreate.vue
@@ -1,6 +1,31 @@
 <script setup lang="ts">
 import BaseTemplate from '../../components/templates/BaseTemplate.vue';
+import { ref, Ref } from 'vue';
+import axios from 'axios';
 
+const name: Ref<string> = ref('');
+const email: Ref<string> = ref('');
+const phone_number: Ref<string> = ref('');
+
+
+const addMember = () => {
+  const token = sessionStorage.getItem('token');
+  const data = {
+    name: name.value,
+    email: email.value,
+    phone_number: phone_number.value
+  }
+  axios.post('http://localhost:8000/users/', data, {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  }).then((res) => {
+    console.log(res)
+  }).catch((err) => {
+    console.log(err)
+  }
+  )
+}
 </script>
 
 <template>
@@ -16,17 +41,22 @@ import BaseTemplate from '../../components/templates/BaseTemplate.vue';
         <v-container class="container">
           <v-row>
             <v-col cols="6">
-              <v-text-field label="名前" outlined dense />
+              <v-text-field label="名前" outlined dense v-model="name" />
             </v-col>
           </v-row>
           <v-row>
             <v-col cols="6">
-              <v-text-field label="メールアドレス" outlined dense />
+              <v-text-field label="メールアドレス" outlined dense v-model="email" />
             </v-col>
           </v-row>
           <v-row>
             <v-col cols="6">
-              <v-btn color="primary" class="float-right">
+              <v-text-field label="電話番号" outlined dense v-model="phone_number" />
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="6">
+              <v-btn color="primary" class="float-right" @click="addMember">
                 追加
               </v-btn>
             </v-col>
